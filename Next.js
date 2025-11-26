f2a42add93b4db0d0425e01766ea295daa6938a5
solpayana <blackguydoodoo@yahoo.com>
2025-11-26 07:14:32 -0500

    Next.js

--- Next.js
+++ Next.js
@@ -1,1 +1,3 @@
-
+npx create-next-app@latest solgrind --ts --tailwind --eslint --app --src-dir
+cd solgrind
+npm install @solana/wallet-adapter-react @solana/wallet-adapter-wallets @solana/wallet-adapter-react-ui @solana/web3.js lucide-react
